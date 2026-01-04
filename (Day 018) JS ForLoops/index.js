5fe26d59257bede2082cc90ec09b7b88167715c7
TegarSukaZeta <nataliasuparmi27@gmail.com>
2026-01-04 19:05:32 +0700

    Day 18

---
+++ (Day 018) JS ForLoops/index.js
@@ -0,0 +1,29 @@
+// For loop = mengulang bebeapa kode dengan jumlah waktu terbatas
+
+for(let i = 0; i <= 2; i++) { // mengulang loop sebanyak 3x ( 0, 1 ,2 )
+    console.log(i); // Tampilkan output i
+}
+
+// penjelasan
+// i = variabel counter
+// let i = 0, adalah posisi default value atau nilai awal variabel i (0 dalam koding adalah angka 1)
+// jika i kurang dari sama dengan (<=) 2,
+// perintahkan variable i untuk membuat increment sehingga variabel i nantinya mendapatkan value / nilai 2
+// (++) adalah increment, fungsinya adalah menambahkan nilai variabel sejumlah 1.
+// (+=2) adalah increment, bedanya dia menambahkan nilai variabel sejumlah 2.
+
+
+///////////////////////////////////////////////////
+
+for(let k = 10; k > 0; k--){ //mengulang loop sebanyak 10x. 
+    console.log(k); //hasil output k
+}
+
+// penjelasan
+// k = variabel counter
+// let k = 10, adalah value yang sudah saya set, bernilai 10
+// di atas mengapa saya bilang mengulang loop sebanyak 10x? karena ini adalah decrement,
+// kebalikan dari increment, decrement adalah proses pengurangan dalam javascript
+// karena valuenya saya set 10, dan sistem decrementnya berkurang 1 demi 1, (10, 9, 8, 7, 6, 5, 4, 3, 2, 1).
+
+// (--) adalah decrement, fungsinya untuk mengurangi nilai variabel sejumlah 1.
